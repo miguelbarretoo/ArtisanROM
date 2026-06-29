bfd2773e406032f39718c17517d946d85f66ed72
Miguel Barreto <miguel03barreto@gmail.com>
2026-06-28 23:14:31 -0300

    bump

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -2,12 +2,12 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Only the below variable(s) need to be changed!
-VERSION_MAJOR=3
-VERSION_MINOR=5
+VERSION_MAJOR=4
+VERSION_MINOR=0
 VERSION_PATCH=0
 
 # The below variables will be generated automatically
 #
 # Version name
 ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
-ROM_CODENAME="Quant"
+ROM_CODENAME="Aether"
